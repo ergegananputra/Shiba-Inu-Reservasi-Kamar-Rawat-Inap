426b0aafd6a98fd862b964ed9224ea2ad9eb4070
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-05-26 00:26:21 +0700

    add v1 fdk

--- models/fasilitas_detail_kamar.py
+++ models/fasilitas_detail_kamar.py
@@ -1,7 +1,7 @@
 from models.room_models import *;
 
 class FasilitasDetailKamar():
-    __table_name__ = "fasilitas_detail_kamar"
+    __table_name__ = "fasilitas_detail_kamar.py"
     __table_args__ = {'extend_existing': True}
 
     id = Column(UUIDType(binary=False), primary_key= True, default=uuid.uuid4, unique=True, nullable=False)
